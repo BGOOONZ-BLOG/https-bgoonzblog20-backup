1abefb6d855fadde92df6a11620994eabead2255
Bryan C Guner <bryan.guner@gmail.com>
2022-06-12 12:13:48 +0000

    update

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++
@@ -1,5 +0,0 @@
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
-}
-
--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,10 +1,4 @@
 module.exports = [{
-      plugin: require('../node_modules/gatsby-remark-prismjs-copy-button/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../node_modules/gatsby-remark-code-buttons/gatsby-browser.js'),
-      options: {"plugins":[],"buttonContainerClass":"customButtonContainerClass","buttonClass":"customButtonClass","buttonText":"customButtonText","svgIconClass":"customSvgIconClass","svgIcon":"customSvgIcon","tooltipText":"customTooltipText","toasterClass":"customToasterClass","toasterTextClass":"customToasterTextClass","toasterText":"customToasterText","toasterDuration":5000},
-    },{
       plugin: require('../gatsby-browser.js'),
       options: {"plugins":[]},
     },{
