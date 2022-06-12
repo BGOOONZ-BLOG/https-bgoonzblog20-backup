6ec915829b35bb4df77b5f0e9405c85a0b120a6e
Bryan C Guner <bryan.guner@gmail.com>
2022-06-12 12:49:29 +0000

    update

--- public/js/inject.js
+++ public/js/inject.js
@@ -1,14 +1,14 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
-! function(e, t) {
+!(function (e, t) {
     if ('object' == typeof exports && 'undefined' != typeof module) {
         module.exports = t();
     } else {
-        'function' == typeof define && define.amd ? define(t) : (e = 'undefined' != typeof globalThis ? globalThis : e || self).reframe = t();
+        'function' == typeof define && define.amd ? define(t) : ((e = 'undefined' != typeof globalThis ? globalThis : e || self).reframe = t());
     }
-}(this, function() {
+})(this, function () {
     'use strict';
-    return function(e, t) {
+    return function (e, t) {
         var i;
         var n;
         var o = 'string' == typeof e ? document.querySelectorAll(e) : e;
@@ -19,13 +19,20 @@
             if (-1 !== f.className.split(' ').indexOf(r) || -1 < f.style.width.indexOf('%')) return;
             var l = f.getAttribute('height') || f.offsetHeight;
             var s = f.getAttribute('width') || f.offsetWidth;
-            var a = ('string' == typeof l ? parseInt(l) : l) / ('string' == typeof s ? parseInt(s) : s) * 100;
+            var a = (('string' == typeof l ? parseInt(l) : l) / ('string' == typeof s ? parseInt(s) : s)) * 100;
             var p = document.createElement('div');
             p.className = r;
             var u = p.style;
-            u.position = 'relative', u.width = '100%', u.paddingTop = ''.concat(a, '%');
+            (u.position = 'relative'), (u.width = '100%'), (u.paddingTop = ''.concat(a, '%'));
             var h = f.style;
-            h.position = 'absolute', h.width = '100%', h.height = '100%', h.left = '0', h.top = '0', null !== (i = f.parentNode) && void 0 !== i && i.insertBefore(p, f), null !== (n = f.parentNode) && void 0 !== n && n.removeChild(f), p.appendChild(f);
+            (h.position = 'absolute'),
+                (h.width = '100%'),
+                (h.height = '100%'),
+                (h.left = '0'),
+                (h.top = '0'),
+                null !== (i = f.parentNode) && void 0 !== i && i.insertBefore(p, f),
+                null !== (n = f.parentNode) && void 0 !== n && n.removeChild(f),
+                p.appendChild(f);
         }
     };
 });
