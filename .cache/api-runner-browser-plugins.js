698c75d42abdc19e8ec34fd1efe3df5e36a37791
Bryan C Guner <bryan.guner@gmail.com>
2022-08-27 05:03:59 +0000

    hi

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../node_modules/gatsby/dist/internal-plugins/partytown/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
